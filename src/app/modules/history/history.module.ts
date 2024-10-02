0dad6af52efa015dca4c4243bece45c9d22e1f5d
Romario Rodriguez <romariorodriguez17@gmail.com>
2024-10-02 14:49:18 -0500

    feat(files) Se añaden archivos de rutas y compartidos

---
+++ src/app/modules/history/history.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HistoruRoutingModule } from './history-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HistoruRoutingModule
+  ]
+})
+export class HistoruModule { }
